888f8c1013fbf19ff6e31d316e6c850d895daf4c
jacmertz <jacmertz@ksu.edu>
2024-11-06 21:17:46 -0600

    v0.5.1

--- pages/index.vue
+++ pages/index.vue
@@ -18,6 +18,8 @@ import '../assets/fancy.css';
 				<p>
 					<b>Lorem ipsum odor amet</b>, <i>consectetuer adipiscing elit.</i> Accumsan gravida mollis fringilla nascetur quam. Tempor semper risus sagittis maximus libero. Ullamcorper odio pulvinar finibus mus nam in convallis nisl. Ligula sit varius elit eros cursus vulputate. Praesent vulputate massa orci ad, fermentum habitant habitasse. Lacus dictum fermentum penatibus; aenean facilisi torquent. Tellus turpis amet euismod tortor augue lacus. Dignissim malesuada rutrum vel sed montes senectus.
 				</p>
+                <br/>
+                <a href="/about">About Me</a> | <a href="/resume">Resume</a>
 			</div>
 		</body>
 	</PrimePanel>
