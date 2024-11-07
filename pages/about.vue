95a8929399ece503833b8e86aa7ea031d55b395d
jacmertz <jacmertz@ksu.edu>
2024-11-06 19:37:04 -0600

    v0.5

--- pages/about.vue
+++ pages/about.vue
@@ -1,6 +1,27 @@
 <template>
+	<title>About</title>
 	<PrimePanel header="About Me" class="my-6">
-		About me stuffs
-		<PrimeButton label="Check" icon="pi pi-check" />
+		<body>
+			<div>
+				<h2 id="underline">My Favorite Movies</h2>
+				<ol>
+					<li>The Hobbit: An Unexpected Journey</li>
+					<li>The Hobbit: The Desolation of Smaug</li>
+					<li>The Hobbit: The Battle of the Five Armies</li>
+					<li>The Lord of the Rings: The Fellowship of the Rings</li>
+					<li>The Lord of the Rings: The Two Towers</li>
+					<li>The Lord of the Rings: The Return of the King</li>
+				</ol>
+				<br/>
+				<a href="https://twitch.tv" target="_blank">
+					My Favorite Link
+				</a>
+				<br/>
+				<br/>
+				<img src="https://upload.wikimedia.org/wikipedia/en/b/b0/Remington_Covers_for_Ballantine_Lord_of_the_Rings_1960s.jpg" alt="The Lord of the Rings Books">
+				<br/>
+				<iframe width="560" height="315" src="https://www.youtube.com/embed/V75dMMIW2B4" frameborder="0" allowfullscreen></iframe>
+			</div>
+		</body>
 	</PrimePanel>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,22 +1,24 @@
-<style>
-@import '../assets/fancy.css';
-</style>
+<script>
+import '../assets/fancy.css';
+</script>
 <template>
 	<title>Home</title>
-	<PrimePanel header="About Me">
-		<body>
-			<p>
-				<b>Lorem ipsum odor amet</b>, <i>consectetuer adipiscing elit.</i> Accumsan gravida mollis fringilla nascetur quam. Tempor semper risus sagittis maximus libero. Ullamcorper odio pulvinar finibus mus nam in convallis nisl. Ligula sit varius elit eros cursus vulputate. Praesent vulputate massa orci ad, fermentum habitant habitasse. Lacus dictum fermentum penatibus; aenean facilisi torquent. Tellus turpis amet euismod tortor augue lacus. Dignissim malesuada rutrum vel sed montes senectus.
-			</p>
-			<p>
-				<b>Lorem ipsum odor amet</b>, <i>consectetuer adipiscing elit.</i> Accumsan gravida mollis fringilla nascetur quam. Tempor semper risus sagittis maximus libero. Ullamcorper odio pulvinar finibus mus nam in convallis nisl. Ligula sit varius elit eros cursus vulputate. Praesent vulputate massa orci ad, fermentum habitant habitasse. Lacus dictum fermentum penatibus; aenean facilisi torquent. Tellus turpis amet euismod tortor augue lacus. Dignissim malesuada rutrum vel sed montes senectus.
-			</p>
-			<p>
-				<b>Lorem ipsum odor amet</b>, <i>consectetuer adipiscing elit.</i> Accumsan gravida mollis fringilla nascetur quam. Tempor semper risus sagittis maximus libero. Ullamcorper odio pulvinar finibus mus nam in convallis nisl. Ligula sit varius elit eros cursus vulputate. Praesent vulputate massa orci ad, fermentum habitant habitasse. Lacus dictum fermentum penatibus; aenean facilisi torquent. Tellus turpis amet euismod tortor augue lacus. Dignissim malesuada rutrum vel sed montes senectus.
-			</p>
-			<p>
-				<b>Lorem ipsum odor amet</b>, <i>consectetuer adipiscing elit.</i> Accumsan gravida mollis fringilla nascetur quam. Tempor semper risus sagittis maximus libero. Ullamcorper odio pulvinar finibus mus nam in convallis nisl. Ligula sit varius elit eros cursus vulputate. Praesent vulputate massa orci ad, fermentum habitant habitasse. Lacus dictum fermentum penatibus; aenean facilisi torquent. Tellus turpis amet euismod tortor augue lacus. Dignissim malesuada rutrum vel sed montes senectus.
-			</p>
+	<PrimePanel header="Home">
+		<body class="background">
+			<div>
+				<p>
+					<b>Lorem ipsum odor amet</b>, <i>consectetuer adipiscing elit.</i> Accumsan gravida mollis fringilla nascetur quam. Tempor semper risus sagittis maximus libero. Ullamcorper odio pulvinar finibus mus nam in convallis nisl. Ligula sit varius elit eros cursus vulputate. Praesent vulputate massa orci ad, fermentum habitant habitasse. Lacus dictum fermentum penatibus; aenean facilisi torquent. Tellus turpis amet euismod tortor augue lacus. Dignissim malesuada rutrum vel sed montes senectus.
+				</p>
+				<p>
+					<b>Lorem ipsum odor amet</b>, <i>consectetuer adipiscing elit.</i> Accumsan gravida mollis fringilla nascetur quam. Tempor semper risus sagittis maximus libero. Ullamcorper odio pulvinar finibus mus nam in convallis nisl. Ligula sit varius elit eros cursus vulputate. Praesent vulputate massa orci ad, fermentum habitant habitasse. Lacus dictum fermentum penatibus; aenean facilisi torquent. Tellus turpis amet euismod tortor augue lacus. Dignissim malesuada rutrum vel sed montes senectus.
+				</p>
+				<p>
+					<b>Lorem ipsum odor amet</b>, <i>consectetuer adipiscing elit.</i> Accumsan gravida mollis fringilla nascetur quam. Tempor semper risus sagittis maximus libero. Ullamcorper odio pulvinar finibus mus nam in convallis nisl. Ligula sit varius elit eros cursus vulputate. Praesent vulputate massa orci ad, fermentum habitant habitasse. Lacus dictum fermentum penatibus; aenean facilisi torquent. Tellus turpis amet euismod tortor augue lacus. Dignissim malesuada rutrum vel sed montes senectus.
+				</p>
+				<p>
+					<b>Lorem ipsum odor amet</b>, <i>consectetuer adipiscing elit.</i> Accumsan gravida mollis fringilla nascetur quam. Tempor semper risus sagittis maximus libero. Ullamcorper odio pulvinar finibus mus nam in convallis nisl. Ligula sit varius elit eros cursus vulputate. Praesent vulputate massa orci ad, fermentum habitant habitasse. Lacus dictum fermentum penatibus; aenean facilisi torquent. Tellus turpis amet euismod tortor augue lacus. Dignissim malesuada rutrum vel sed montes senectus.
+				</p>
+			</div>
 		</body>
 	</PrimePanel>
 </template>
