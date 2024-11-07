b38c280501e7f3723f4770b959f5bf29452d9990
jacmertz <jacmertz@ksu.edu>
2024-11-06 18:15:32 -0600

    v0.1.3

--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,21 @@
 <style>
-@import './assets/fancy.css';
+@import '../assets/fancy.css';
 </style>
 <template>
 	<title>Home</title>
 	<PrimePanel header="About Me">
 		<body>
 			<p>
-				<b>Lorem</b> ipsum odor amet, consectetuer adipiscing elit. Accumsan gravida mollis fringilla nascetur quam. Tempor semper risus sagittis maximus libero. Ullamcorper odio pulvinar finibus mus nam in convallis nisl. Ligula sit varius elit eros cursus vulputate. Praesent vulputate massa orci ad, fermentum habitant habitasse. Lacus dictum fermentum penatibus; aenean facilisi torquent. Tellus turpis amet euismod tortor augue lacus. Dignissim malesuada rutrum vel sed montes senectus.
+				<b>Lorem ipsum odor amet</b>, <i>consectetuer adipiscing elit.</i> Accumsan gravida mollis fringilla nascetur quam. Tempor semper risus sagittis maximus libero. Ullamcorper odio pulvinar finibus mus nam in convallis nisl. Ligula sit varius elit eros cursus vulputate. Praesent vulputate massa orci ad, fermentum habitant habitasse. Lacus dictum fermentum penatibus; aenean facilisi torquent. Tellus turpis amet euismod tortor augue lacus. Dignissim malesuada rutrum vel sed montes senectus.
 			</p>
 			<p>
-				<b>Lorem</b> ipsum odor amet, consectetuer adipiscing elit. Accumsan gravida mollis fringilla nascetur quam. Tempor semper risus sagittis maximus libero. Ullamcorper odio pulvinar finibus mus nam in convallis nisl. Ligula sit varius elit eros cursus vulputate. Praesent vulputate massa orci ad, fermentum habitant habitasse. Lacus dictum fermentum penatibus; aenean facilisi torquent. Tellus turpis amet euismod tortor augue lacus. Dignissim malesuada rutrum vel sed montes senectus.
+				<b>Lorem ipsum odor amet</b>, <i>consectetuer adipiscing elit.</i> Accumsan gravida mollis fringilla nascetur quam. Tempor semper risus sagittis maximus libero. Ullamcorper odio pulvinar finibus mus nam in convallis nisl. Ligula sit varius elit eros cursus vulputate. Praesent vulputate massa orci ad, fermentum habitant habitasse. Lacus dictum fermentum penatibus; aenean facilisi torquent. Tellus turpis amet euismod tortor augue lacus. Dignissim malesuada rutrum vel sed montes senectus.
 			</p>
 			<p>
-				<b>Lorem</b> ipsum odor amet, consectetuer adipiscing elit. Accumsan gravida mollis fringilla nascetur quam. Tempor semper risus sagittis maximus libero. Ullamcorper odio pulvinar finibus mus nam in convallis nisl. Ligula sit varius elit eros cursus vulputate. Praesent vulputate massa orci ad, fermentum habitant habitasse. Lacus dictum fermentum penatibus; aenean facilisi torquent. Tellus turpis amet euismod tortor augue lacus. Dignissim malesuada rutrum vel sed montes senectus.
+				<b>Lorem ipsum odor amet</b>, <i>consectetuer adipiscing elit.</i> Accumsan gravida mollis fringilla nascetur quam. Tempor semper risus sagittis maximus libero. Ullamcorper odio pulvinar finibus mus nam in convallis nisl. Ligula sit varius elit eros cursus vulputate. Praesent vulputate massa orci ad, fermentum habitant habitasse. Lacus dictum fermentum penatibus; aenean facilisi torquent. Tellus turpis amet euismod tortor augue lacus. Dignissim malesuada rutrum vel sed montes senectus.
 			</p>
 			<p>
-				<b>Lorem</b> ipsum odor amet, consectetuer adipiscing elit. Accumsan gravida mollis fringilla nascetur quam. Tempor semper risus sagittis maximus libero. Ullamcorper odio pulvinar finibus mus nam in convallis nisl. Ligula sit varius elit eros cursus vulputate. Praesent vulputate massa orci ad, fermentum habitant habitasse. Lacus dictum fermentum penatibus; aenean facilisi torquent. Tellus turpis amet euismod tortor augue lacus. Dignissim malesuada rutrum vel sed montes senectus.
+				<b>Lorem ipsum odor amet</b>, <i>consectetuer adipiscing elit.</i> Accumsan gravida mollis fringilla nascetur quam. Tempor semper risus sagittis maximus libero. Ullamcorper odio pulvinar finibus mus nam in convallis nisl. Ligula sit varius elit eros cursus vulputate. Praesent vulputate massa orci ad, fermentum habitant habitasse. Lacus dictum fermentum penatibus; aenean facilisi torquent. Tellus turpis amet euismod tortor augue lacus. Dignissim malesuada rutrum vel sed montes senectus.
 			</p>
 		</body>
 	</PrimePanel>
